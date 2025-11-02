d34122118d6ab72362747e4063a3acc1eee8cf6c
nxtDandelion <stanislavkasmak6@gmail.com>
2025-11-02 14:24:29 +0300

    10003: Auth-Service - базовое принятие на / и /health

--- Auth Service/main.py
+++ Auth Service/main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+@app.get("/health")
+async def health():
+    return {"message": "healthy"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
