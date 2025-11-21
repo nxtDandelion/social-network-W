f77d4542ec26746c00377c3292aeef6036710ba8
dzhichko <dzhichko.ao@edu.spbstu.ru>
2025-11-21 23:15:17 +0300

    добавление перепаковки jwt токена

--- Gateway/src/main/java/com/socialw/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/socialw/gateway/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.socialw.gateway.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,5 +11,9 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
