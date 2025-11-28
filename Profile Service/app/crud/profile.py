2130a16a5fc36b56b11255bf8cf15845174a03ac
mkonchev <maksim1901m@gmail.com>
2025-11-29 02:43:03 +0300

    add post_create logic

--- Profile Service/app/crud/profile.py
+++ Profile Service/app/crud/profile.py
@@ -1,6 +1,6 @@
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, update
 # from sqlalchemy import select, update, delete
 from app.schemas import profile as schemas
 from app.database import models
@@ -24,6 +24,12 @@ class ProfileCRUD:
     async def get_profile(self, username: str) -> models.Profile:
         result = await self.db.execute(
             select(models.Profile).where(models.Profile.username == username)
+        )
+        return result.scalar_one_or_none()
+
+    async def get_profile_by_id(self, id: str) -> models.Profile:
+        result = await self.db.execute(
+            select(models.Profile).where(models.Profile.uuid == id)
         )
         return result.scalar_one_or_none()
 
@@ -54,9 +60,47 @@ class ProfileCRUD:
         await self.db.refresh(db_profile)
         return db_profile
 
+    async def update_profile_by_id(
+        self,
+        id: str,
+        profile_update: schemas.ProfileUpdate
+    ):
+        db_profile = await self.get_profile_by_id(id)
+        if not db_profile:
+            return None
+        update_data = profile_update.model_dump(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(db_profile, field, value)
+        await self.db.commit()
+        await self.db.refresh(db_profile)
+        return db_profile
+
     async def delete_profile(self, username: str) -> models.Profile:
         db_profile = await self.get_profile(username)
         if db_profile:
             await self.db.delete(db_profile)
             await self.db.commit()
+        return db_profile
+
+    async def add_post_to_profile(self, profile_uuid: str, post_id: int):
+        db_profile = await self.db.get(models.Profile, profile_uuid)
+
+        if db_profile:
+            current_posts = db_profile.user_posts
+
+            if current_posts is None:
+                current_posts = []
+
+            if post_id not in current_posts:
+                current_posts.append(post_id)
+
+            stmt = (
+                update(models.Profile)
+                .where(models.Profile.uuid == profile_uuid)
+                .values(user_posts=current_posts)
+            )
+            await self.db.execute(stmt)
+            await self.db.commit()
+            await self.db.refresh(db_profile)
+
         return db_profile
